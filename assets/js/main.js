ab0423689025ebdd37500e2d81933cde2fa635d2
Abdallah Salah <abdallah.salah.13811@gmail.com>
2022-08-31 00:09:41 +0200

    about ......weekly

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+
+const nav = document.querySelector('nav');
+const stats = document.querySelector('.stats');
+const statsContainer = document.querySelectorAll('.state-num');
+let start = false; 
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 30) {
+    nav.classList.add('active');
+  } else {
+    nav.classList.remove('active');
+  }
+
+  if (window.scrollY >= stats.offsetTop ) {
+    if (!start) {
+      statsContainer.forEach(el => {
+        let endNum = parseInt(el.dataset.target);
+        let num = 0;
+        
+        let numInterval = setInterval(() => {
+          num++;
+          el.innerHTML = num;
+          if (num === endNum) {
+            clearInterval(numInterval);
+          }
+        } ,2000 / endNum);
+      } );
+    }
+    start = true;
+  }
+} );
